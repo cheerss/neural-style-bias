a43d1ca47967e4a942831f7691561f0b9afca2fe
cheerss <zjdxwwx@163.com>
2017-03-24 11:35:49 +0800

    run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 ##morph 2-4
 
-python neural_style.py --content 'examples/1-content.jpg' --styles 'examples/3-style.jpg' --output 'examples/origin-13/output.jpg' --iterations 800 --checkpoint-iterations 50 --checkpoint-output 'examples/origin-13/output%s.jpg'
-python neural_style.py --content 'examples/1-content.jpg' --styles 'examples/4-style.jpg' --output 'examples/origin-14/output.jpg' --iterations 800 --checkpoint-iterations 50 --checkpoint-output 'examples/origin-14/output%s.jpg'
-python neural_style.py --content 'examples/1-content.jpg' --styles 'examples/5-style.jpg' --output 'examples/origin-15/output.jpg' --iterations 800 --checkpoint-iterations 50 --checkpoint-output 'examples/origin-15/output%s.jpg'
-python neural_style.py --content 'examples/1-content.jpg' --styles 'examples/6-style.jpg' --output 'examples/origin-16/output.jpg' --iterations 800 --checkpoint-iterations 50 --checkpoint-output 'examples/origin-16/output%s.jpg'
+# python neural_style.py --content 'examples/1-content.jpg' --styles 'examples/3-style.jpg' --output 'examples/origin-13/output.jpg' --iterations 800 --checkpoint-iterations 50 --checkpoint-output 'examples/origin-13/output%s.jpg'
+# python neural_style.py --content 'examples/1-content.jpg' --styles 'examples/4-style.jpg' --output 'examples/origin-14/output.jpg' --iterations 800 --checkpoint-iterations 50 --checkpoint-output 'examples/origin-14/output%s.jpg'
+# python neural_style.py --content 'examples/1-content.jpg' --styles 'examples/5-style.jpg' --output 'examples/origin-15/output.jpg' --iterations 800 --checkpoint-iterations 50 --checkpoint-output 'examples/origin-15/output%s.jpg'
+# python neural_style.py --content 'examples/1-content.jpg' --styles 'examples/6-style.jpg' --output 'examples/origin-16/output.jpg' --iterations 800 --checkpoint-iterations 50 --checkpoint-output 'examples/origin-16/output%s.jpg'
 
 python neural_style.py --content 'examples/2-content.jpg' --styles 'examples/3-style.jpg' --output 'examples/origin-23/output.jpg' --iterations 800 --checkpoint-iterations 50 --checkpoint-output 'examples/origin-23/output%s.jpg'
 python neural_style.py --content 'examples/3-content.jpg' --styles 'examples/4-style.jpg' --output 'examples/origin-34/output.jpg' --iterations 800 --checkpoint-iterations 50 --checkpoint-output 'examples/origin-34/output%s.jpg'
