d5ca27c81f87d9354164ae73a19fde1bb3920543
cheers <zjdxwwx@163.com>
2017-05-27 14:15:47 +0800

    d

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 mkdir examples/0527
 
-CUDA_VISIBLE_DEVICES=2 python neural_style.py --content 'examples/tubingen.jpg' --styles 'examples/1-style.jpg' --iterations 800 --output 'examples/0527/t1.jpg'
-CUDA_VISIBLE_DEVICES=2 python neural_style.py --content 'examples/tubingen.jpg' --styles 'examples/7-style.jpg' --iterations 800 --output 'examples/0527/t7.jpg'
-CUDA_VISIBLE_DEVICES=2 python neural_style.py --content 'examples/tubingen.jpg' --styles 'examples/8-style.jpg' --iterations 800 --output 'examples/0527/t8.jpg'
+CUDA_VISIBLE_DEVICES=2 python neural_style.py --content 'examples/tubingen.jpg' --styles 'examples/1-style.jpg' --iterations 150 --output 'examples/0527/t1-150.jpg'
+CUDA_VISIBLE_DEVICES=2 python neural_style.py --content 'examples/tubingen.jpg' --styles 'examples/7-style.jpg' --iterations 150 --output 'examples/0527/t7-150.jpg'
+CUDA_VISIBLE_DEVICES=2 python neural_style.py --content 'examples/tubingen.jpg' --styles 'examples/8-style.jpg' --iterations 150 --output 'examples/0527/t8-150.jpg'
 
-CUDA_VISIBLE_DEVICES=2 python neural_style.py --content 'examples/tubingen.jpg' --styles 'examples/9-style.jpg' --iterations 800 --output 'examples/0527/t9.jpg'
+CUDA_VISIBLE_DEVICES=2 python neural_style.py --content 'examples/tubingen.jpg' --styles 'examples/9-style.jpg' --iterations 150 --output 'examples/0527/t9-150.jpg'
