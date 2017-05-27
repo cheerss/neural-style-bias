6d2c10fede0cc10f458ffb226bd010bde5f37e8e
cheers <zjdxwwx@163.com>
2017-05-27 12:01:29 +0800

    d

--- run.sh
+++ run.sh
@@ -1,12 +1,7 @@
-##morph 2-4
+mkdir examples/0527
 
-# python neural_style.py --content 'examples/1-content.jpg' --styles 'examples/3-style.jpg' --output 'examples/origin-13/output.jpg' --iterations 800 --checkpoint-iterations 50 --checkpoint-output 'examples/origin-13/output%s.jpg'
-# python neural_style.py --content 'examples/1-content.jpg' --styles 'examples/4-style.jpg' --output 'examples/origin-14/output.jpg' --iterations 800 --checkpoint-iterations 50 --checkpoint-output 'examples/origin-14/output%s.jpg'
-# python neural_style.py --content 'examples/1-content.jpg' --styles 'examples/5-style.jpg' --output 'examples/origin-15/output.jpg' --iterations 800 --checkpoint-iterations 50 --checkpoint-output 'examples/origin-15/output%s.jpg'
-# python neural_style.py --content 'examples/1-content.jpg' --styles 'examples/6-style.jpg' --output 'examples/origin-16/output.jpg' --iterations 800 --checkpoint-iterations 50 --checkpoint-output 'examples/origin-16/output%s.jpg'
+CUDA_VISIBLE_DEVICES=2 python neural_style.py --content 'examples/4-content.jpg' --styles 'examples/5-style.jpg' --iterations 800 --output 'examples/0527/45.jpg'
+CUDA_VISIBLE_DEVICES=2 python neural_style.py --content 'examples/fran.jpeg' --styles 'examples/6-style.jpg' --iterations 800 --output 'examples/0527/f6.jpg'
+CUDA_VISIBLE_DEVICES=2 python neural_style.py --content 'examples/2-content.jpg' --styles 'examples/3-style.jpg' --iterations 800 --output 'examples/0527/23.jpg'
 
-python neural_style.py --content 'examples/2-content.jpg' --styles 'examples/3-style.jpg' --output 'examples/origin-23/output.jpg' --iterations 800 --checkpoint-iterations 50 --checkpoint-output 'examples/origin-23/output%s.jpg'
-python neural_style.py --content 'examples/3-content.jpg' --styles 'examples/4-style.jpg' --output 'examples/origin-34/output.jpg' --iterations 800 --checkpoint-iterations 50 --checkpoint-output 'examples/origin-34/output%s.jpg'
-python neural_style.py --content 'examples/4-content.jpg' --styles 'examples/5-style.jpg' --output 'examples/origin-45/output.jpg' --iterations 800 --checkpoint-iterations 50 --checkpoint-output 'examples/origin-45/output%s.jpg'
-python neural_style.py --content 'examples/5-content.jpeg' --styles 'examples/6-style.jpg' --output 'examples/origin-56/output.jpg' --iterations 800 --checkpoint-iterations 50 --checkpoint-output 'examples/origin-56/output%s.jpg'
-
+CUDA_VISIBLE_DEVICES=2 python neural_style.py --content 'examples/1-content.jpg' --styles 'examples/4-style.jpg' --iterations 800 --output 'examples/0527/14.jpg'
