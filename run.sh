0765b197b152f17cea633d507fadafb813b86e54
cheers <zjdxwwx@163.com>
2017-05-29 10:46:02 +0800

    d

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 
 
-CUDA_VISIBLE_DEVICES=2 python neural_style.py --content 'examples/tubingen.jpg' --styles 'examples/sumiao.jpg' --iterations 150 --output 'examples/output-1.jpg'
-CUDA_VISIBLE_DEVICES=2 python neural_style.py --content 'examples/4-content.jpg' --styles 'examples/sumiao.jpg' --iterations 150 --output 'examples/output-2.jpg'
-CUDA_VISIBLE_DEVICES=2 python neural_style.py --content 'examples/1-content.jpg' --styles 'examples/sumiao.jpg' --iterations 150 --output 'examples/output-3.jpg'
+CUDA_VISIBLE_DEVICES=0 python neural_style.py --content 'examples/tubingen.jpg' --styles 'examples/sumiao.jpg' --iterations 150 --output 'examples/output-1.jpg'
+CUDA_VISIBLE_DEVICES=0 python neural_style.py --content 'examples/4-content.jpg' --styles 'examples/sumiao.jpg' --iterations 150 --output 'examples/output-2.jpg'
+CUDA_VISIBLE_DEVICES=0 python neural_style.py --content 'examples/1-content.jpg' --styles 'examples/sumiao.jpg' --iterations 150 --output 'examples/output-3.jpg'
