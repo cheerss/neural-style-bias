89071a59722643324907a497aee4b958f26f26a9
cheers <zjdxwwx@163.com>
2017-06-06 09:50:12 +0800

    d

--- stylize.py
+++ stylize.py
@@ -11,7 +11,9 @@ from sys import stderr
 from PIL import Image
 
 CONTENT_LAYERS = ('relu4_2', 'relu5_2')
-STYLE_LAYERS = ('relu1_1', 'relu2_1', 'relu3_1', 'relu4_1', 'relu5_1')
+# STYLE_LAYERS = ('relu1_1', 'relu2_1', 'relu3_1', 'relu4_1', 'relu5_1')
+STYLE_LAYERS = ('relu1_1')
+
 
 try:
     reduce
