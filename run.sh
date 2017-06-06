00a295153f70f74e74fa3f56f3466fe1dd543829
cheers <zjdxwwx@163.com>
2017-06-06 09:21:44 +0800

    d

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
+mkdir examples/0606
 
+CUDA_VISIBLE_DEVICES=2 python neural_style.py --content 'examples/tubingen.jpg' --styles 'examples/1-style.jpg' --iterations 150 --output 'examples/0606/1.jpg'
+CUDA_VISIBLE_DEVICES=2 python neural_style.py --content 'examples/4-content.jpg' --styles 'examples/2-style1.jpg' --iterations 150 --output 'examples/0606/2.jpg'
+CUDA_VISIBLE_DEVICES=2 python neural_style.py --content 'examples/1-content.jpg' --styles 'examples/7-style.jpg' --iterations 150 --output 'examples/0606/7.jpg'
+CUDA_VISIBLE_DEVICES=2 python neural_style.py --content 'examples/1-content.jpg' --styles 'examples/8-style.jpg' --iterations 150 --output 'examples/0606/8.jpg'
+CUDA_VISIBLE_DEVICES=2 python neural_style.py --content 'examples/1-content.jpg' --styles 'examples/3-style.jpg' --iterations 150 --output 'examples/0606/3.jpg'
+CUDA_VISIBLE_DEVICES=2 python neural_style.py --content 'examples/1-content.jpg' --styles 'examples/4-style.jpg' --iterations 150 --output 'examples/0606/4.jpg'
+CUDA_VISIBLE_DEVICES=2 python neural_style.py --content 'examples/1-content.jpg' --styles 'examples/5-style.jpg' --iterations 150 --output 'examples/0606/5.jpg'
+CUDA_VISIBLE_DEVICES=2 python neural_style.py --content 'examples/1-content.jpg' --styles 'examples/9-style.jpg' --iterations 150 --output 'examples/0606/9.jpg'
 
-CUDA_VISIBLE_DEVICES=2 python neural_style.py --content 'examples/tubingen.jpg' --styles 'examples/sumiao.jpg' --iterations 800 --output 'examples/output-1.jpg'
-CUDA_VISIBLE_DEVICES=2 python neural_style.py --content 'examples/4-content.jpg' --styles 'examples/sumiao.jpg' --iterations 800 --output 'examples/output-2.jpg'
-CUDA_VISIBLE_DEVICES=2 python neural_style.py --content 'examples/1-content.jpg' --styles 'examples/sumiao.jpg' --iterations 800 --output 'examples/output-3.jpg'
