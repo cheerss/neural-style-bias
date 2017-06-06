3cc18eeaf5b57790235b866d090680450eca4773
cheers <zjdxwwx@163.com>
2017-06-06 10:18:51 +0800

    d

--- stylize.py
+++ stylize.py
@@ -12,7 +12,7 @@ from PIL import Image
 
 CONTENT_LAYERS = ('relu4_2', 'relu5_2')
 # STYLE_LAYERS = ('relu1_1', 'relu2_1', 'relu3_1', 'relu4_1', 'relu5_1')
-STYLE_LAYERS = ('relu2_1',)
+STYLE_LAYERS = ('relu3_1',)
 
 
 try:
